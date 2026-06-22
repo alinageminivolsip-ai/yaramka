92499c6f93619074b0f12ae2d81a2114b67f6675
alinageminivolsip-ai <alinageminivolsip@gmail.com>
2026-06-22 16:26:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'framefit-3d-v1';
+const CACHE_NAME = 'framefit-3d-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate', event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));self.clients.claim();});
